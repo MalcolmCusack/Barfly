c50eacf863a72229cdd75b05eceef7794fd5806e
jesserussell <jesserussell411@gmail.com>
2021-11-19 23:42:04 -0700

    Revert "Merge branch 'adding-typescript'"
    This reverts commit 0922f1aa086c0d040c2a38f59fcbf4706a88f5d3, reversing
    changes made to 09eceb2d3804825006468e2d93732d6e7f8358de.

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ import Welcome from './components/welcome';
 // Back end push: amplify push
 // Front end push: git push <branch> or origin master
 
-//adding a comment to merge again cuase im retarded and reverted the last pull request
-
 const theme = createTheme({
   palette: {
     primary: {
