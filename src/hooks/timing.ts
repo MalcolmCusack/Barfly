2985050fcb3af7b6dcda87689f87c4eec713daea
jesserussell <jesserussell411@gmail.com>
2021-11-21 17:23:01 -0700

    added react component-safe timeout and interval functions

---
+++ src/hooks/timing.ts
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+
+/**
+ * @returns a knock-off of the setTimeout function that's safe to use in react components.
+ * Any timeouts created will be automatically cleared when the calling component de-renders.
+ */
+export function useTimeout() {
+    // id list
+    const [IDs] = useState([] as NodeJS.Timeout[]);
+
+    // destructor
+    //     clears all timeouts
+    useEffect(
+        () => () => {
+            for (const id of IDs) {
+                clearTimeout(id);
+            }
+        },
+        []
+    );
+
+    return function reactTimeout(
+        callback: (...args: any[]) => void,
+        ms: number,
+        ...args: any[]
+    ) {
+        const id = setTimeout(callback, ms, ...args);
+        IDs.push(id);
+        return id;
+    };
+}
+
+/**
+ * @returns a knock-off of the setInterval function that's safe to use in react components.
+ * Any intervals created will be automatically cleared when the calling component de-renders.
+ */
+export function useInterval() {
+    // id list
+    const [IDs] = useState([] as NodeJS.Timeout[]);
+
+    // destructor
+    //     clears all intervals
+    useEffect(
+        () => () => {
+            for (const id of IDs) {
+                clearInterval(id);
+            }
+        },
+        []
+    );
+
+    return function reactInterval(
+        callback: (...args: any[]) => void,
+        ms: number,
+        ...args: any[]
+    ) {
+        const id = setInterval(callback, ms, ...args);
+        IDs.push(id);
+        return id;
+    };
+}
