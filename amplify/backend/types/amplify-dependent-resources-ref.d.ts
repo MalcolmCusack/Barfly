32e6299fc79ede2dc923ae35f3a7f9b84bec0bdc
Malcolm Cusack <cusackmalk@gmail.com>
2021-11-27 00:09:30 -0700

    Added lamba function for auth post confirmation user mutation to DB

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,16 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "Barfly": {
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
     "storage": {
         "barflystorage": {
             "BucketName": "string",
             "Region": "string"
+        }
+    },
+    "function": {
+        "CreateUser": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     }
 }
