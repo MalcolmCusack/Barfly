ec6aa37d1394e81a5d74dabf6ca937ac48ed3364
mroduin44 <mroduin44@gmail.com>
2021-12-01 15:31:02 -0700

    Implementation of Pourhouse scrape and push to transfer to my other device

---
+++ src/python_files/pourhouse.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+location = "Pourhouse"
+log_mode = 1
+log = []
+log_file = "A:/Fall_2021/CSCI_331/Barfly/Barfly/src/python_files/log_files/"+datetime.datetime.now().strftime("%d_%b_%Y")+".txt"
+output_location = "A:/Fall_2021/CSCI_331/Barfly/Barfly/src/scraped_files/pourhouse.txt"
+
+def time():
+    return datetime.datetime.now().strftime("%d/%b/%Y %H:%M:%S")
+
+def pourhouse_food():
+
+    log.append(time() + " Starting Pourhouse Food Function")
+
+    output = []
+    
+    URL = "https://www.phbozeman.com/"
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    
+    items = soup.find_all("div", "menu-item")
+    for item in items:
+        name = "Food"
+        title = item.find("div", "menu-item-title").text.strip()
+        description = item.find("div", "menu-item-description")
+        if(description == None):
+            log.append(time()+ " ERROR: Description unavailible for " + title)
+            description = ""
+        else:
+            description = description.text.strip()
+        price = item.find("span", "menu-item-price-top")
+        if(price == None):
+            title = title.split("|")
+            price = title[1].strip()
+            title = title[0].strip()
+        else:
+            price = price.text.strip()
+        if(description != None and price != None):
+            output.append("|".join([location, name, title, price, description]))
+
+    log.append(time()+ " Finished Pourhouse Food Function")
+    return output
+
+file = open(output_location, "w")
+for item in pourhouse_food():
+    file.write(item + "\n")
+file.close()
+
+if(log_mode):
+    file = open(log_file, "a")
+    for item in log:
+        file.write(item + "\n")
+    file.close()
+
+print("Pourhouse Scan Completed")
